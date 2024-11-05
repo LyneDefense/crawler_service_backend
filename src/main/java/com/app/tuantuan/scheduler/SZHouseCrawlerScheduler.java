6b4bdce081e300f110dcb8d9c75df561c5fddda5
pinjhu <hpj@xkool.info>
2024-11-06 00:05:52 +0800

    改了个bug

--- src/main/java/com/app/tuantuan/scheduler/SZHouseCrawlerScheduler.java
+++ src/main/java/com/app/tuantuan/scheduler/SZHouseCrawlerScheduler.java
@@ -55,7 +55,7 @@ public class SZHouseCrawlerScheduler {
   }
 
   /** 一手房源公示首页信息爬取定时任务：每天凌晨1点，除了周三 */
-  @@Scheduled(cron = "0 0 1 * * 1-3,5-7")
+  @Scheduled(cron = "0 0 1 * * 1-3,5-7")
   public void crawNewHouseMainPageItemsAndBuildingsDaily() {
     StopWatch stopWatch = new StopWatch();
     stopWatch.start();
